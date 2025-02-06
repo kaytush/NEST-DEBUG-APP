3f692317bdd0d595650ee2406540a29647abcc6a
Kayode Omotosho <kaytush@yahoo.com>
2025-02-06 22:58:15 +0100

    changed X  port to 8080

--- src/debug-env.controller.ts
+++ src/debug-env.controller.ts
@@ -12,9 +12,10 @@ export class DebugEnvController {
     // Return specific keys or all environment variables
     return {
       NODE_ENV: process.env.NODE_ENV,
-      NEXT_PUBLIC_BASE_URL: process.env.NEXT_PUBLIC_BASE_URL,
-      NEXT_PUBLIC_SYNC_KEY: process.env.NEXT_PUBLIC_SYNC_KEY,
-      TEMP: process.env.TEMP, // Windows TEMP directory
+    //   NEXT_PUBLIC_BASE_URL: process.env.NEXT_PUBLIC_BASE_URL,
+    //   NEXT_PUBLIC_SYNC_KEY: process.env.NEXT_PUBLIC_SYNC_KEY,
+    //   TEMP: process.env.TEMP, // Windows TEMP directory
+      PORT: process.env.PORT
       // You can add other keys here as needed for debugging
     };
 
